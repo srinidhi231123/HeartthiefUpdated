585ce1066bc1c74257b85e62fa1aa2f90206070f
Hᴇᴀʀᴛ_Tʜɪᴇꜰᴛ <151960878+Jeeva1055221@users.noreply.github.com>
2024-10-07 14:24:17 +0530

    Update info.py

--- info.py
+++ info.py
@@ -63,7 +63,7 @@ TEXT = environ.get("APPROVED_WELCOME_TEXT", "<b>{mention},\n\nʏᴏᴜʀ ʀᴇǫ
 APPROVED = environ.get("APPROVED_WELCOME", "on").lower()
 
 # Others
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 SHORTLINK_URL = environ.get('SHORTLINK_URL', 'gplinks.com')
 SHORTLINK_API = environ.get('SHORTLINK_API', '89c5f49bb30bf45ddae5d35314cc7525a36aceb0')
 IS_SHORTLINK = bool(environ.get('IS_SHORTLINK',True))
